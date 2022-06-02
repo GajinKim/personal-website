cdbfd27a1adb0744d063b9c08400c8c136adb1f0
GaJin Kim <gajin2000@gmail.com>
2022-06-02 17:42:16 -0500

    created skeleton for homepage

---
+++ src/pages/HomePage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import '../styles/HomePage.css';
+ 
+const HomePage = () => {
+  return (
+    <div className='homepage-wrapper'>
+        <div className='homepage-content-wrapper'>
+            <p>Hi, my name is</p>
+            <h1>Wonder.</h1>
+            <h1>I'm a Software Engineer.</h1>
+            <p>I"m a 22 year old chad</p>
+        </div>
+    </div>
+  );
+}
+ 
+export default HomePage;
